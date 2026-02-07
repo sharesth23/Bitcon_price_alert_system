ecbbea3bee20a0f32f79d59994c925c03c315d3f
Sharesth Gulia <sharesth@Sharesths-MacBook-Air.local>
2026-02-07 16:27:05 +0530

    added a new class named as BitcoinTracker

--- bitcoin_tracker.py
+++ bitcoin_tracker.py
@@ -15,4 +15,24 @@ try:
     NOTIFICATIONS_AVAILABLE = True
 except ImportError:
     NOTIFICATIONS_AVAILABLE = False
-    
+
+import threading
+
+class BitcoinTracker:
+    def __init__(self , db_path:  str = "bitcoin_tracker.db"):
+        self.db_path = db_path 
+        self.apis = {
+            'coindesk': 'https://api.coindesk.com/v1/bpi/currentprice.json',
+            'coinbase': 'https://api.coinbase.com/v2/prices/BTC-{currency}/spot',
+            'blockchain': 'https://blockchain.info/ticker',
+            'coingecko': 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies={currency}'
+
+        }
+
+        self.supported_currencies = ['USD', 'EUR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'HKD', 'NZD', 'INR']
+        self.current_currency = 'USD'
+        self.currency_symbols = {
+            'USD': '$', 'EUR': '€', 'GBP': '£', 'JPY': '¥',
+            'AUD': 'A$', 'CAD': 'C$', 'CHF': 'Fr', 'CNY': '¥', 'INR': '₹'
+        }
+        

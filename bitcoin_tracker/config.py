fd197eca0fdf209c8e761e71cf7393a6583fc7e7
sharesth23 <sharesth444yt@gmail.com>
2026-02-08 04:12:42 +0530

    added db path and apis to config file

---
+++ bitcoin_tracker/config.py
@@ -0,0 +1,7 @@
+DB_PATH = "bitcoin_tracker.db"
+
+APIS = {
+    "coindesk": "https://api.coindesk.com/v1/bpi/currentprice.json",
+    "coinbase": "https://api.coinbase.com/v2/prices/BTC-USD/spot",
+    "blockchain": "https://blockchain.info/ticker",
+}

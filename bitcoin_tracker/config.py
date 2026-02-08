7d1febc515656264fddf7736a42fc4421cdb7651
sharesth23 <sharesth444yt@gmail.com>
2026-02-08 10:02:02 +0530

    added threshold , currency symbols , interval seconds

--- bitcoin_tracker/config.py
+++ bitcoin_tracker/config.py
@@ -5,3 +5,17 @@ APIS = {
     "coinbase": "https://api.coinbase.com/v2/prices/BTC-USD/spot",
     "blockchain": "https://blockchain.info/ticker",
 }
+ALERT_THRESHOLD = {
+        "minor" : 2.0,
+        "major" : 5.0,
+        "critical" : 10.0
+}
+
+DEFAULT_INTERVAL_SECONDS  = 60 
+CURRECY_SYMBOLS = {
+     "USD": "$",
+    "EUR": "€",
+    "INR": "₹",
+    "GBP": "£",
+    "JPY": "¥",
+}

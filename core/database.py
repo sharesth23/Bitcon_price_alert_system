cb2f19575870555f5ae65a0b97716c44d786608f
sharesth23 <sharesth444yt@gmail.com>
2026-02-09 23:29:52 +0530

    added a new function for databse management

--- core/database.py
+++ core/database.py
@@ -1,3 +1,15 @@
 import sqlite3
 import time
 from bitcoin_tracker.config import DB_PATH
+
+class Database:
+    def __init__(self, path=DB_PATH):
+        self.path = path
+        self._init_db()
+
+    def _connect(self):
+        return sqlite3.connect(self.path)
+
+    def _init_db(self):
+        conn = self._connect()
+        cur = conn.cursor()
